d2af88d724223597bb4a25f343f1eb6e81ae8193
root <root@UDAY>
2024-11-23 14:18:15 +0530

    Added github and linkedin icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import { FaDiscord,FaGithub ,FaLinkedin, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
+  { href: "https://github.com/uday778", icon: <FaGithub  /> },
+  { href: "https://www.linkedin.com/in/uday778/", icon: <FaLinkedin/> },
 ];
 
 const Footer = () => {
@@ -12,7 +11,7 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          @Uday Kumar. All rights reserved
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
